672204eeea9637c72daf407bdd44fa6dd7d06710
Howard Chu <howardchu95@gmail.com>
2025-10-23 15:30:33 -0700

    stachu: Fix UB when using erase() in set
    Signed-off-by: Howard Chu <howardchu95@gmail.com>

--- stachu.cc
+++ stachu.cc
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <set>
+#include <map>
 
 #define CYAN "\x1b[;36m"
 #define RESET "\x1b[;39m"
 
 static const std::string rm = "rm ";
-using task_set_t = std::set<std::pair<unsigned long long, std::string>>;
+using task_map_t = std::map<unsigned long long, std::string>;
 
 static bool starts_with(const std::string& str, const std::string& prefix)
 {
@@ -23,17 +23,17 @@ static bool starts_with(const std::string& str, const std::string& prefix)
 #define print_coloredl(stuff) \
     std::cout << CYAN << stuff << RESET;
 
-static void print_tasks(task_set_t &task_set)
+static void print_tasks(task_map_t &task_map)
 {
     unsigned long long fake_idx = 1;
-    for (auto iter = task_set.rbegin(); iter != task_set.rend(); ++iter) {
+    for (auto iter = task_map.rbegin(); iter != task_map.rend(); ++iter) {
         print_coloredl(fake_idx << ". " << CYAN << iter->second);
         std::cout << std::endl;
         ++fake_idx;
     }
 }
 
-static void operate(std::string& user_input, task_set_t &task_set, unsigned long long &cnt,
+static void operate(std::string& user_input, task_map_t &task_map, unsigned long long &cnt,
                     unsigned long long &completed)
 {
     if (starts_with(user_input, rm)) {
@@ -49,18 +49,17 @@ static void operate(std::string& user_input, task_set_t &task_set, unsigned long
             std::cout << "std::stoull failed" << std::endl;
         }
 
-        for (auto iter = task_set.rbegin(); iter != task_set.rend(); ++iter) {
-            if (idx == user_idx - 1) {
-                task_set.erase(std::next(iter).base());
-                removed = true;
-                break;
-            }
-            idx += 1;
+        if (user_idx <= task_map.size())  {
+            auto it = task_map.begin();
+            advance(it, task_map.size() - user_idx);
+            task_map.erase(it);
+            removed = true;
         }
+
         if (!removed) {
             std::cout << "task of \'" << user_idx << "\' doesn't exist" << std::endl;
         } else {
-            print_tasks(task_set);
+            print_tasks(task_map);
             completed += 1;
         }
     } else if (user_input.size() == 0 || (user_input.size() > 0 && std::isspace(user_input[0]))) {
@@ -68,13 +67,13 @@ static void operate(std::string& user_input, task_set_t &task_set, unsigned long
         print_coloredl(completed << " completed");
         std::cout << std::endl;
     } else if (user_input == "p") {
-        print_tasks(task_set);
+        print_tasks(task_map);
     } else if (user_input == "q") {
         exit(0);
     } else {
-        task_set.insert(make_pair(cnt, user_input));
+        task_map.insert(make_pair(cnt, user_input));
         cnt += 1;
-        print_tasks(task_set);
+        print_tasks(task_map);
     }
 }
 
@@ -82,15 +81,14 @@ static int start()
 {
     unsigned long long cnt = 0ULL;
     unsigned long long completed = 0;
-    task_set_t task_set;
+    task_map_t task_map;
     std::string user_input;
     auto prompt = []() {
         std::cout << "stachu > " << std::flush;
     };
-
     prompt();
     while (std::getline(std::cin, user_input)) {
-        operate(user_input, task_set, cnt, completed);
+        operate(user_input, task_map, cnt, completed);
         prompt();
     }
     return 0;
